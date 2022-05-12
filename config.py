dc150870490b48a28165dc638d91c6cf6402e0fb
MercenaryHarpy6 <79670342+MercenaryHarpy6@users.noreply.github.com>
2022-05-11 19:06:50 -0500

    configmachine

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
 #configuration file for set up
-#
+
+#configuration for Server Machine (shared netwrok file)
+ShareFile = ''
+Sharepass = '' #password for server account
+
+#configuration for local machine ()
+
