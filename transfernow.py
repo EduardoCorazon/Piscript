7e78fabf1896c929a76eec3b71ac636d2f696c60
MercenaryHarpy6 <79670342+MercenaryHarpy6@users.noreply.github.com>
2022-05-12 19:12:49 -0500

    update 2

--- transfernow.py
+++ transfernow.py
@@ -12,15 +12,17 @@ import sys
 #consider using paramiko?
 
 def serversetup():
-    login = subprocess.run("ssh {user}@{host} {cmd}".format(user=RemoteUser, host=RemotePass, cmd='ls -l'), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    #login = subprocess.run("ssh {user}@{host} {cmd}".format(user=RemoteUser, host=RemotePass, cmd='ls -l'), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     
     
 
     #checks if there was an error
+    '''
     if login.returncode != 0:
         print("\nfThere was an error")
         print(login.stderr)
-    #subprocess.run(['powershell.exe', 'New-SmbShare', '-Path', 'E:\DSC\ ', '-Name', ShareFile]) # creates a share file on Server PC
+    '''
+    subprocess.run(['powershell.exe', 'New-SmbShare', '-Path', 'C:\\Users\\ ', '-Name', 'ShareFile']) # creates a share file on Server PC
 
 
 
