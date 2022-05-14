cadc03bd9b1ab28ded5ee7473d82d3bb52649c47
MercenaryHarpy6 <79670342+MercenaryHarpy6@users.noreply.github.com>
2022-05-13 20:24:57 -0500

    test1

--- transfernow.py
+++ transfernow.py
@@ -9,25 +9,22 @@ Description: Simple script that logs into my pi via ssh and runs whatever I want
 import subprocess
 
 
-
 def info():
     info.RemoteUser = str(input("Input Remote machine username: "))
-    info.RemotePass = str(input("Input Remote machine password: "))
+    info.RemoteIP = str(input("Input Remote machine IP: "))
+    info.target = str(input("Input the target IP: "))
     print("If no commands are passed in, an nmap scan and metsploit will be started; if a common services is found running then it will be searched in metasploit")
-    info.command = str(input("input command you want to run here [for default leave empty]:"))
+    info.command = str(input("input command you want to run here [for default leave empty]: \n"))
+    if (info.command == ''):
+        info.command = "msfconsole && nmap -Pn {target}".format(target=info.target)
+    return(info.command)
     
 
 def loginserver():
-    login = subprocess.run("ssh {user}@{host} {cmd}".format(user=info.RemoteUser, host=info.RemotePass, cmd='nmap -A '), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    
-
-
-
-
-
-
+    login = subprocess.run("ssh {user}@{host} {cmd}".format(user=info.RemoteUser, host=info.RemoteIP, cmd=info.command), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    print(login.stdout)
 
 if __name__ == '__main__':
     info()
-    #loginserver()
+    loginserver()
     
