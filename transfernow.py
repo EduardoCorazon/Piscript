12d9a01c1827550f1a3ad83d9de427321b840a9d
MercenaryHapy6 <corazoneduardo@att.net>
2022-05-14 16:35:38 -0500

    update

--- transfernow.py
+++ transfernow.py
@@ -5,7 +5,8 @@ Description: Simple script that logs into my pi via ssh and runs whatever I want
 '''
 #!/usr/bin/env python3
 
-#imports
+# imports
+from config import *
 import subprocess
 import itertools
 from threading import Thread
@@ -13,20 +14,40 @@ import time
 import sys
 
 
+def localdetail():
+    # check if windows or linux
+    if sys.platform == "linux" OR "linux2"
+    localinfo = subprocess.run("ifconfig -a | grep 'broadcast'", shell=True,
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    print("Your network information is: \n" +
+          localinfo.stdout + "You are running: ")
+
+
 def info():
-    info.RemoteUser = str(input("Input Remote machine username: "))
-    info.RemoteIP = str(input("Input Remote machine IP: "))
+    if username != '':
+        print("Using configfile...\n")
+        info.RemoteUser = username
+        info.RemoteIP = remotehost
+
+    else:
+
+        info.RemoteUser = str(input("Input Remote machine username: "))
+        info.RemoteIP = str(input("Input Remote machine IP: "))
     info.target = str(input("Input the target IP: "))
     print("If no commands are passed in, an nmap scan will be performed")
-    info.command = str(input("input command you want to run here [for default leave empty]:"))
+    info.command = str(
+        input("input command you want to run here [for default leave empty]:"))
     defaultcommand()
-    
-    
+
+
 def defaultcommand():
     if (info.command == ''):
-        print("An nmap scan will now be done\nPlease wait while the scan is being performed")
-        info.command = "msfconsole && nmap -A -vv {target}".format(target=info.target)
+        print(
+            "An nmap scan will now be done\nPlease wait while the scan is being performed")
+        info.command = "msfconsole && nmap -A -vv {target}".format(
+            target=info.target)
     return(info.command)
+
 
 def waitanimation():
     if loginserver.waiting == False:
@@ -38,15 +59,17 @@ def waitanimation():
             sys.stdout.flush()
             time.sleep(0.1)
 
+
 def loginserver():
     loginserver.waiting = False
-    login = subprocess.run("ssh {user}@{host} {cmd}".format(user=info.RemoteUser, host=info.RemoteIP, cmd=info.command), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    login = subprocess.run("ssh {user}@{host} {cmd}".format(user=info.RemoteUser, host=info.RemoteIP,
+                           cmd=info.command), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     loginserver.waiting = True
     print(login.stdout)
 
-if __name__ == '__main__':
-    info()
-    Thread(target = loginserver).start()
-    Thread(target = waitanimation).start()
 
-    
+if __name__ == '__main__':
+    localdetail()
+    info()
+    Thread(target=loginserver).start()
+    Thread(target=waitanimation).start()
