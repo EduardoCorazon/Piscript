c37f1d5897baaf1ccc37b2f865ebb8ed1aeac703
MercenaryHarpy6 <79670342+MercenaryHarpy6@users.noreply.github.com>
2022-05-14 12:30:05 -0500

    mainpartdone

--- transfernow.py
+++ transfernow.py
@@ -7,24 +7,46 @@ Description: Simple script that logs into my pi via ssh and runs whatever I want
 
 #imports
 import subprocess
+import itertools
+from threading import Thread
+import time
+import sys
 
 
 def info():
     info.RemoteUser = str(input("Input Remote machine username: "))
     info.RemoteIP = str(input("Input Remote machine IP: "))
     info.target = str(input("Input the target IP: "))
-    print("If no commands are passed in, an nmap scan and metsploit will be started; if a common services is found running then it will be searched in metasploit")
-    info.command = str(input("input command you want to run here [for default leave empty]: \n"))
-    if (info.command == ''):
-        info.command = "msfconsole && nmap -Pn {target}".format(target=info.target)
-    return(info.command)
+    print("If no commands are passed in, an nmap scan will be performed")
+    info.command = str(input("input command you want to run here [for default leave empty]:"))
+    defaultcommand()
     
+    
+def defaultcommand():
+    if (info.command == ''):
+        print("An nmap scan will now be done\nPlease wait while the scan is being performed")
+        info.command = "msfconsole && nmap -A -vv {target}".format(target=info.target)
+    return(info.command)
+
+def waitanimation():
+    if loginserver.waiting == False:
+        for i in itertools.cycle(["⢿", "⣻", "⣽", "⣾", "⣷", "⣯", "⣟", "⡿"]):
+            if loginserver.waiting == True:
+                print("done!")
+                break
+            sys.stdout.write('\rloading ' + i)
+            sys.stdout.flush()
+            time.sleep(0.1)
 
 def loginserver():
+    loginserver.waiting = False
     login = subprocess.run("ssh {user}@{host} {cmd}".format(user=info.RemoteUser, host=info.RemoteIP, cmd=info.command), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    loginserver.waiting = True
     print(login.stdout)
 
 if __name__ == '__main__':
     info()
-    loginserver()
+    Thread(target = loginserver).start()
+    Thread(target = waitanimation).start()
+
     
