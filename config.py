8bfe5bc38285d4f21eb69467df39d35162aa59bf
MercenaryHarpy6 <79670342+MercenaryHarpy6@users.noreply.github.com>
2022-05-29 12:18:34 -0500

    Final commit

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 '''
 MAKES CHANGES TO THIS FILE, ADD YOUR USERNAME/PASSWORD
-This is a configuration file to store the Username, password/key for the SSH connection
+This is a configuration file to store the Username and password for the SSH connection
+Depending on your configuration you could be using an ssh key pair in which case the password won't be needed
 NOTE: This the main file can run without this file but it just makes it easier to keep/use this config file for automation purposes 
 '''
 
 username = '' #input the username of the remote host (aka the Pi)
 remotehost = ''#input the IP address of the remote host
 password = ''#input the password, if you're using ssh key you don't need this
-sshkeylocation = '' #Input the file location of your private ssh key
